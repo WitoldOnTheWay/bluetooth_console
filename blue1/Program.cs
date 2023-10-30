01bb26ba7e27371b1f6509a285d9a85b6a13ac82
Witold <witold.sko@gmail.com>
2023-10-30 13:47:05 +0100

    initial commit

---
+++ blue1/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace blue1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("link inital project to repo");
+        }
+    }
+}
